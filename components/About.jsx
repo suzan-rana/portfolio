d9a380c9331b3ca1b3c98716041c5418a52ced0f
Suzan Rana <dev.suzanrana@gmail.com>
2022-10-20 16:21:44 +0545

    fixed title:

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import me from "../public/images/me.jpg";
 import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
-
+import { IoIosArrowForward } from "react-icons/io";
 
 const copyText = () => {
   const tag = document.getElementById("copyField");
@@ -20,26 +20,44 @@ const About = () => {
       </h1>
       <div className="flex flex-col md:flex-row flex-start gap-6 md:gap-16 my-16 py-12 rounded-lg px-8 bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg dark:bg-slate-800 ">
         <div className="overflow-hidden rounded-xl">
-          <Image src={me} className="block min-h-full max-w-[100%]" alt="suzarnanainpicture"/>
+          <Image
+            src={me}
+            className="block min-h-full max-w-[100%]"
+            alt="suzarnanainpicture"
+          />
         </div>
-
-        <p className="text-xl max-w-[500px] pt-4  leading-8 dark:text-slate-300" id="contact">
-          Hello, my name is Suzan Rana, a developer based in Nepal. I love
-          programming and building web applications and solving the problems of
-          multiple people through technology. Every time I solve a new problem
-          and help people and grow business, it makes my heart warm and glad and
-          the sense of satisfaction of that particular moment keeps me moving
-          forward.
-        </p>
+        <div className="max-w-[500px]">
+          <h3 className="mb-4 tracking-widest text-3xl font-clashDisplay font-semibold flex">
+            
+            Who am I ?
+          </h3>
+          <p className="text-xl  leading-8 dark:text-slate-300" id="contact">
+            Hello, my name is Suzan Rana, a developer based in Nepal. I love
+            programming and building web applications and solving the problems
+            of multiple people through technology. Every time I solve a new
+            problem and help people and grow business, it makes my heart warm
+            and glad and the sense of satisfaction of that particular moment
+            keeps me moving forward.
+          </p>
+        </div>
       </div>
       <div className="flex  my-6 justify-around gap-4 items-center bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg dark:bg-slate-800 min-w-[300px] w-[50%] px-5 py-6 mx-auto rounded-xl ">
-        <a href="" className="inline-block"><AiFillLinkedin  className="text-6xl "/></a>
-        <a href="https://github.com/suzan-rana" rel="noreferrer" target="_blank" className="inline-block"><AiFillGithub className="text-6xl"/></a>
-        <a href="" className="inline-block"><AiFillTwitterCircle className="text-6xl"/></a>
-    
-        
+        <a href="" className="inline-block">
+          <AiFillLinkedin className="text-6xl " />
+        </a>
+        <a
+          href="https://github.com/suzan-rana"
+          rel="noreferrer"
+          target="_blank"
+          className="inline-block"
+        >
+          <AiFillGithub className="text-6xl" />
+        </a>
+        <a href="" className="inline-block">
+          <AiFillTwitterCircle className="text-6xl" />
+        </a>
       </div>
-      
+
       <div className="text-center text-[#337308] text-3xl underline font-clashDisplay cursor-pointer">
         <a href="mailto:dev.suzan@gmail.com">Click here to open in Mail.</a>
       </div>
