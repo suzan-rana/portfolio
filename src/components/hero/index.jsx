bdf95646b273e1354af496ffd219e22d1209663b
Suzan Rana <dev.suzanrana@gmail.com>
2023-06-22 21:14:23 +0545

    removed pattern

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -4,7 +4,7 @@ import Pattern from "./Pattern";
 const Hero = () => {
   return (
     <section id="hero" className="relative z-10">
-      <Pattern />
+      {/* <Pattern /> */}
       <div className="sm:pt-[12rem] pt-[6rem] relative">
         <Info />
       </div>
